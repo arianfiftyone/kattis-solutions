0eb2fd049617f3c0887ae329f65814f046e62d02
arianfiftyone <arian.masoudi@hotmail.com>
2022-02-04 20:28:54 +0100

    initial commit

---
+++ Tarifa.py
@@ -0,0 +1,16 @@
+def getMegabytes(mb, used):
+    leftoverMegabytes = mb - used
+
+
+
+
+
+def availableMegabytes():
+    X = int(input())
+    N = int(input())
+    P = int(input())
+    pass
+
+
+if __name__ == '__main__':
+    availableMegabytes()
